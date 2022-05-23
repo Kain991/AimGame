61225b7eaa6f564eb11b02e043a5b18f50c118b7
Kain991 <kainarberkin03012003@gmail.com>
2022-05-23 16:38:13 +0600

    finishgame , settime , startgame modified

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const screens = document.querySelectorAll('.screen');
 
 const timeList = document.querySelector('#time-list');
 
+const timeEl = document.querySelector('#time');
+
+let time = 11;
+
 startBtn.addEventListener('click', (event) => {
     event.preventDefault();
 screens[0].classList.add('up');
@@ -13,12 +17,40 @@ timeList.addEventListener('click' , event =>
 {
     if(event.target.classList.contains('time-btn')) {
         time = parseInt(event.target.getAttribute('data-time'));
+        screens[1].classList.add('up');
         startGame();
     }
 
 })
 
+//DDEBUG 
+startGame();
+
 function startGame(){
-    screens[1].classList.add('up');
+    setInterval(decreaseTime , 1000);
+    timeEl.innerHTML = `00:${time}`;
 }
 
+function decreaseTime(){
+    if(time === 0){
+        finishGame();
+    }
+    else{
+        let current = --time;
+        setTime(current);
+    }
+    
+}
+
+function setTime(value){
+    if(value < 10){
+        timeEl.innerHTML = `00:0${value}`
+    }
+    else {
+        timeEl.innerHTML = `00:${value}`;
+    }
+}
+
+function finishGame(){
+    
+}
