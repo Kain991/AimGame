f63881f60f126915ae5a45dc87b753a737f5cfc8
Kain991 <kainarberkin03012003@gmail.com>
2022-05-28 15:02:17 +0600

    last finish commit

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ const timeList = document.querySelector('#time-list');
 
 const timeEl = document.querySelector('#time');
 
-let time = 11;
+const board = document.querySelector('#board');
+
+const colors = ['#242F9B' , '#646FD4' , '#9BA3EB' , '#DBDFFD' ]
+let time = 0;
+let score = 0;
 
 startBtn.addEventListener('click', (event) => {
     event.preventDefault();
 screens[0].classList.add('up');
 })
+
+function getRandomColor(){
+    const index = Math.floor(Math.random()*colors.length);
+    return colors[index];
+}
+
 
 timeList.addEventListener('click' , event =>
 {
@@ -23,11 +33,19 @@ timeList.addEventListener('click' , event =>
 
 })
 
-//DDEBUG 
-startGame();
+board.addEventListener('click' , event =>{
+    if(event.target.classList.contains('circle')){
+        score++;
+        event.target.remove();
+        createRandomCircle();
+    }
+} )
+
+
 
 function startGame(){
     setInterval(decreaseTime , 1000);
+    createRandomCircle();
     timeEl.innerHTML = `00:${time}`;
 }
 
@@ -52,5 +70,28 @@ function setTime(value){
 }
 
 function finishGame(){
-    
+    timeEl.parentNode.classList.add('hide');
+    board.innerHTML = `<h1> Ваш счет :<span class="primary"> ${score} </span></h1>`;
+}
+
+function createRandomCircle(){
+    const circle = document.createElement('div');
+    const size = getRandomNumber(20 , 80);
+    const {width, height} = board.getBoundingClientRect()
+    const x = getRandomNumber(0 , width-size);
+    const y = getRandomNumber(0 , height -size);
+
+    circle.classList.add('circle');
+    circle.style.width = `${size}px`;
+    circle.style.height = `${size}px`;
+    circle.style.top = `${y}px`;
+    circle.style.left = `${x}px`;
+    circle.style.background = `${getRandomColor()}`;
+
+    board.append(circle)
+
+}
+
+function getRandomNumber(max , min){
+    return Math.round( Math.random() * ( max - min ) + min );
 }
